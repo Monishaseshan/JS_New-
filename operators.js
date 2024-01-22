26faba7ddb762a4b4e3c90119f1631d2f1a90215
Monishaseshan <monishas.21ece@kongu.edu>
2024-01-22 19:23:59 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,44 @@
+/*
+//assignment operators
+var a=10;
+console.log("Assignment operators:",a)
+
+//arithmetic operator
+
+var aA = 10
+var bA = 10
+console.log(typeof(aA))
+console.log("Arithmetic operator addition:",aA+bA)
+console.log("Arithmetic operator subtraction:",aA-bA)
+console.log("Arithmetic operator multiplication:",aA*bA)
+console.log("Arithmetic operator division:",aA/bA)
+console.log("Arithmetic operator modulus:",aA%bA)
+
+//Logical operator -->&&,||,!
+var aA=10
+var bA=10
+console.log("Logical operator && output",aA&&bA)
+console.log("Logical operator || output",aA||bA)
+console.log("Logical operator ! output",!aA)
+
+console.log(10 && 20)
+console.log(10 && 20)
+console.log(10 && 0)
+console.log(0 && 30)
+
+console.log(10 || 20)
+console.log(0 || 20)
+console.log(20 || 0)
+console.log(100 || 20 && 0)
+
+//Relational operator
+
+aR=10
+bR=20
+console.log("Relational operator > is:",aR>bR)
+console.log("Relational operator < is:",aR<bR)
+console.log("Relational operator >= is:",aR>=bR)
+console.log("Relational operator <= is:",aR<=bR)
+console.log("Relational operator === is:",aR===bR)
+console.log("Relational operator !== is:",aR!==bR)
+
